5a6e1bb05d8cb2baf9cc5f22d3b88a7d7c989bc1
koh-suzuki <k.s_coward_0629@icloud.com>
2019-10-11 14:31:09 +0000

    Finish edit-attendances

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -46,4 +46,12 @@ class AttendancesController < ApplicationController
     def attendances_params
       params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
     end
+    
+    def admin_or_correct_user
+      @user = User.find(params[:user_id]) if @user.admin?
+      unless current_user?(@user) || current_user.admin?
+      flash[:danger] = "編集権限がありません。"
+      redirect_to(root_url)
+      end
+    end
 end
