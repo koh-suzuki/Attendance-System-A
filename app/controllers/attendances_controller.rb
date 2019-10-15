e305a80056bfe178bd42831c362a6e32f050d61b
koh-suzuki <k.s_coward_0629@icloud.com>
2019-10-15 11:29:22 +0000

    edit_one_month アクセス制限　追加

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,6 +2,7 @@ class AttendancesController < ApplicationController
   before_action :set_user, only: :edit_one_month
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :set_one_month, only: :edit_one_month
+  before_action :correct_user, only: :edit_one_month
   
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
   
@@ -37,7 +38,7 @@ class AttendancesController < ApplicationController
     end
       flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
       redirect_to user_url(date: params[:date])
-    rescue ActiveRecord::RecordInvalid
+  rescue ActiveRecord::RecordInvalid
       flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
       redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
