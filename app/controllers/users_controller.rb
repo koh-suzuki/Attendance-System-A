68f72adcc2f5a9a0454d76a53ab8b7e37b1b2d29
koh-suzuki <k.s_coward_0629@icloud.com>
2019-10-16 09:08:21 +0000

    test modification all Finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :correct_user, only: [:edit, :show, :update, :edit_basic_info]
+  before_action :correct_user, only: [:edit, :update, :edit_basic_info]
   before_action :admin_user, only: [:index, :destroy, :edit_basic_info, :update_basic_info]
   before_action :set_one_month, only: :show
+  before_action :admin_or_correct_user, only: :show
 
  def index
    @users = User.paginate(page: params[:page])
@@ -57,7 +58,13 @@ class UsersController < ApplicationController
     end
     redirect_to users_url
   end
-
+  
+  def admin_or_correct_user
+    unless current_user?(@user) || current_user.admin?
+      flash[:danger] = "残念でした！"
+      redirect_to(root_url)
+    end
+  end
 
   private
 
