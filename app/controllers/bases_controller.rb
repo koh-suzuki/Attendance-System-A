48e02633f659832fd30a45f36a2da3efc3f5e852
koh-suzuki <k.s_coward_0629@icloud.com>
2019-11-30 14:55:46 +0000

    finish 拠点情報追加

--- app/controllers/bases_controller.rb
+++ app/controllers/bases_controller.rb
@@ -1,4 +1,25 @@
 class BasesController < ApplicationController
   def index
+    @bases = Base.all
   end
+  
+  def new
+    @base = Base.new
+  end
+    
+  def create
+    @base = Base.new(base_params)
+    if @base.save
+      flash[:success] = "拠点情報を作成しました。"
+      redirect_to bases_url
+    else
+      flash[:notice] = "拠点情報の作成に失敗しました。"
+      render :index
+    end
+  end
+  
+  private
+    def base_params
+      params.require(:base).permit(:base_number, :base_name, :base_attendance)
+    end
 end
