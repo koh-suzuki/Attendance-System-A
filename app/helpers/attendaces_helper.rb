43aca62b36ebf69df15734dd8c2e5a56e4405abc
koh-suzuki <k.s_coward_0629@icloud.com>
2020-01-17 08:04:13 +0900

    add we_time

--- app/helpers/attendaces_helper.rb
+++ app/helpers/attendaces_helper.rb
@@ -13,8 +13,8 @@ module AttendacesHelper
   end
   
   # 時間外時間の計算
-  def tomorrow_work_end(work_end_time)
-      format("%.2f", (((work_end_time) / 60) / 60.0))
+  def work_end_time(endtime, finish)
+    format("%.2f", (((endtime - finish) / 60) / 60.0))
   end
   
   def css_class(wo)
