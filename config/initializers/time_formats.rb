071cfb4794f2d574c7e6be8a7d07ad32b4affd35
koh-suzuki <k.s_coward_0629@icloud.com>
2020-01-25 17:16:20 +0900

    finish invalid 修正完了

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -7,5 +7,5 @@
 
 # カスタムフォーマットを定義
 Time::DATE_FORMATS[:published_on] = "%Y年%m月%d日 %H時%M分"
-Date::DATE_FORMATS[:default]      = "%Y年%m月d日"
+Date::DATE_FORMATS[:default]      = "%Y年%m月%d日"
 Date::DATE_FORMATS[:date] = "%m/%d"
