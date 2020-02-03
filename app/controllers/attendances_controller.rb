514633a45a169fd40f0ce2a2eaed8772465aa9ae
koh-suzuki <k.s_coward_0629@icloud.com>
2020-02-03 21:28:07 +0900

    残業申請の通知（改修中）

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,7 +2,8 @@ class AttendancesController < ApplicationController
   include AttendacesHelper
   before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :edit_user_id, only: [:edit_overtime_app, :update_over_app, :edit_notice_overtime, :update_notice_overtime]
-  before_action :set_attendance_id, only: [:edit_overtime_app, :update_over_app, :edit_notice_overtime]
+  # before_action :set_notice, only: [:edit_notice_overtime]
+  before_action :set_attendance_id, only: [:edit_overtime_app, :update_over_app, :edit_notice_overtime, :update_notice_overtime]
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :set_one_month, only: [:edit_one_month, :edit_overtime_app, :edit_notice_overtime, :update_notice_overtime]
   before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
