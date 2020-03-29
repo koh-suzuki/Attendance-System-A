9424696ec7dae0073fef690795f9f774c971f775
koh-suzuki <k.s_coward_0629@icloud.com>
2020-03-29 22:55:53 +0900

    時間外時間 翌日ボタン機能 追加

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -13,8 +13,12 @@ module AttendancesHelper
   end
   
   # 時間外時間の計算
-  def over_time(endtime, designated_work_end_time)
-    format("%.2f", (((Time.parse(endtime) - Time.parse(designated_work_end_time)) / 60) / 60.0))
+  def over_time(endtime, designated_work_end_time, day)
+    if day.tommorow_index == true
+      format("%.2f", (((Time.parse(endtime) - Time.parse(designated_work_end_time)) / 60) / 60.0) + 24)
+    else
+      format("%.2f", (((Time.parse(endtime) - Time.parse(designated_work_end_time)) / 60) / 60.0))
+    end
   end
   
   def css_class(wo)
