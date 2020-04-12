683c6af648ad33fa99af08351995638a1aab5c4e
koh-suzuki <k.s_coward_0629@icloud.com>
2020-04-12 12:47:57 +0900

    残業申請 お知らせボタン共通表示の修正 & 残業申請後、勤怠変更申請お知らせもお知らせボタンが表示される 修正

--- db/schema.rb
+++ db/schema.rb
@@ -66,14 +66,14 @@ ActiveRecord::Schema.define(version: 20200408113114) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "affiliation"
-    t.datetime "basic_time", default: "2020-04-11 08:00:00"
-    t.datetime "work_time", default: "2020-04-11 07:30:00"
+    t.datetime "basic_time", default: "2020-04-12 08:00:00"
+    t.datetime "work_time", default: "2020-04-12 07:30:00"
     t.boolean "superior", default: false
     t.integer "employee_number"
     t.string "uid"
-    t.datetime "designated_work_start_time", default: "2020-04-11 09:00:00"
-    t.datetime "designated_work_end_time", default: "2020-04-11 18:00:00"
-    t.datetime "basic_work_time", default: "2020-04-11 08:00:00"
+    t.datetime "designated_work_start_time", default: "2020-04-12 09:00:00"
+    t.datetime "designated_work_end_time", default: "2020-04-12 18:00:00"
+    t.datetime "basic_work_time", default: "2020-04-12 08:00:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
